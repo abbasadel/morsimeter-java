f62b7414195dee885024e1fdb2f9cb1811abe8b6
aibrahim <abbas.ibrahim@smava.de>
2020-03-15 15:33:41 +0100

    master: use stats

--- src/main/java/com/tahrirsquad/morsimeter/controllers/IndexController.java
+++ src/main/java/com/tahrirsquad/morsimeter/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.tahrirsquad.morsimeter.controllers;
 
+import com.tahrirsquad.morsimeter.services.CategoryService;
+import com.tahrirsquad.morsimeter.services.PromiseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private CategoryService categoryService;
+
+    private PromiseService promiseService;
+
+    public IndexController(CategoryService categoryService, PromiseService promiseService) {
+        this.categoryService = categoryService;
+        this.promiseService = promiseService;
+    }
+
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("name", "ABBAS");
+        model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("stats", promiseService.calcTotalStats());
         return "index";
 
     }
